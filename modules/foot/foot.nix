3ca3acde70ad5a5f96bb982fccc1e0dccb7db398
clouwud <dakshgajbhiye902@gmail.com>
2025-11-12 03:50:56 +0530

    init

--- modules/foot/foot.nix
+++ modules/foot/foot.nix
@@ -6,7 +6,7 @@
       main = {
         font = "Iosevka Nerd Font:style=Regular:pixelsize=18";
         font-bold = "Iosevka Nerd Font:style=Bold:pixelsize=18";
-        pad = "20x20 center";
+        pad = "10x10 center";
       };
       scrollback.lines = 10000;
       colors = {
