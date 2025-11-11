4da0c6561b88983e31d7e8adf197f22f0f7714f3
clouwud <dakshgajbhiye902@gmail.com>
2025-11-11 18:58:17 +0530

    init

--- home/daksh/files.nix
+++ home/daksh/files.nix
@@ -14,5 +14,24 @@
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
+    ".config/uwsm/env".text = ''
+      export QT_AUTO_SCREEN_SCALE_FACTOR=1
+      export QT_QPA_PLATFORM=wayland;xcb
+      export QT_QPA_PLATFORMTHEME=qt5ct
+      export XDG_SESSION_TYPE=wayland
+      export XDG_CURRENT_DESKTOP=Hyprland
+      export XDG_SESSION_DESKTOP=Hyprland
+      export XCURSOR_THEME=Bibata-Modern-Classic
+      export XCURSOR_SIZE=35
+      export LIBVA_DRIVER_NAME=nvidia
+      export __GLX_VENDOR_LIBRARY_NAME=nvidia
+      export HYPRSHOT_DIR=/home/daksh/Pictures/screenshots
+    '';
+
+    ".config/uwsm/env-hyprland".text = ''
+      export HYPRCURSOR_THEME=Bibata-Modern-Classic
+      export HYPRCURSOR_SIZE=35
+      export AQ_DRM_DEVICES="/dev/dri/card1:/dev/dri/card0"
+    '';
   };
 }
