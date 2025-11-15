47dce7cfd0c9c702c39a25ec3186917c62a8798c
clouwud <dakshgajbhiye902@gmail.com>
2025-11-16 02:38:51 +0530

    init

--- hosts/zeus/packages.nix
+++ hosts/zeus/packages.nix
@@ -1,19 +1,13 @@
 {
-    pkgs,
-    ...
+  pkgs,
+  ...
 }:
 {
-  # Install firefox.
-  # programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim
     git
     wget
+    steam-run
+    cyme
   ];
 }
